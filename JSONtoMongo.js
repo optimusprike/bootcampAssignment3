2f04e1622a64b577e73ca4db5181527a654c397b
Prakash Pudhucode <optimusprike@users.noreply.github.com>
2017-09-06 20:30:36 -0400

    Add files via upload

---
+++ JSONtoMongo.js
@@ -0,0 +1,32 @@
+'use strict';
+/* 
+  Import modules/files you may need to correctly run the script. 
+  Make sure to save your DB's uri in the config file, then import it with a require statement!
+ */
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+    jsonEntries = require('./listings')
+
+/* Connect to your database */
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db.uri, { useMongoClient: true });
+/* 
+  Instantiate a mongoose model for each listing object in the JSON file, 
+  and then save it to your Mongo database 
+ */
+ jsonEntries.entries.forEach(function(entry){
+  new Listing({
+    name: entry.name,
+    code: entry.code,
+    coordinates: entry.coordinates,
+    address: entry.address
+  }).save();
+
+
+/* 
+  Once you've written + run the script, check out your MongoLab database to ensure that 
+  it saved everything correctly. 
+ */
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://pudhucodep:123@ds129024.mlab.com:29024/cen3031-assignment3',
+  }
+};
+
+
